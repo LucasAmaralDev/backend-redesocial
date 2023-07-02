75dfdd1da34789e60ceaa086e97ff83e607e0e18
Lucas Amaral <lucasahenriqueamaral@gmail.com>
2023-07-02 00:17:21 -0400

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 require('./database');
 require('dotenv').config();
+var cors = require('cors')
+
 
 const express = require('express');
 const { routes } = require('./routes');
 
 const server = express();
 
+server.use(cors())
 server.use(express.json());
 server.use(routes);
+
 
 server.listen(8080, () => {
     console.log('🚀 Server started!');
